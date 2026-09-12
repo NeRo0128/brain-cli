4afd2d96619f8e6ae419e05fa0e4fed91952f4b0
Nero <castillovegarodolfogilberto@gmail.com>
2026-09-11 20:49:00 -0400

    feat(core/tool): RequiresSudo field

--- internal/core/tool/entity.go
+++ internal/core/tool/entity.go
@@ -28,6 +28,8 @@ type Tool struct {
 	CreatedAt      time.Time  `db:"created_at"      json:"created_at"`
 	UpdatedAt      *time.Time `db:"updated_at"      json:"updated_at,omitempty"`
 	DeletedAt      *time.Time `db:"deleted_at"      json:"deleted_at,omitempty"`
+
+	RequiresSudo bool `db:"requires_sudo" json:"requires_sudo" yaml:"requires_sudo"`
 }
 
 // ToolType define los tipos de scripts soportados
